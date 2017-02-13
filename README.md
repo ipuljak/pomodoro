76376adc3e7f82a3045feb7f194403c6795f27cc
Ivan <ivanpulj@gmail.com>
2017-02-13 00:53:37 -0500

    Added README file and some code documentation

---
+++ README.md
@@ -0,0 +1,15 @@
+#Pomodoro Clock
+
+This is a simple [Pomodoro timer](https://en.wikipedia.org/wiki/Pomodoro_Technique) I made that is written in Javascript with jQuery & HTML/CSS. Set a break and session length, and the clock will count down from the defined setting time. Once it reaches zero, it counts down from the defined break length, and then finally repeats the process. The break and session settings can only be set when the clock is not running.
+
+###How To###
+
+To use this Pomodoro clock, clone the repo and open the index.html file in a browser of your choice.
+
+###Live Demo###
+
+You can try out the live demo [here](https://www.puljak.ca/projects/basic/pomodoro/).
+
+####About Me#####
+
+For more information about me, visit my website at [puljak.ca](https://puljak.ca)!
--- main.js
+++ main.js
@@ -1,100 +1,116 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    var paused = true;
-    var isBreak = false;
-    var curTime = parseInt($("#timer").text());
-    var curBreak = parseInt($("#break").text());
-    var curSession = parseInt($("#session").text());
+  // Instantiate some default values
+  var paused = true;
+  var isBreak = false;
+  var curTime = parseInt($("#timer").text());
+  var curBreak = parseInt($("#break").text());
+  var curSession = parseInt($("#session").text());
 
-    var clock = new Date();
-    clock.setHours(0, curTime, 0);
+  // Set a timer to some amount of minutes
+  var clock = new Date();
+  clock.setHours(0, curTime, 0);
 
-    var t = 0;
+  var t = 0;
 
-    $("#minusBreak").on("click", function() {
-        if (curBreak > 1 && paused) {
-            curBreak--;
-            $("#break").text(curBreak);
-        }
-    });
-
-    $("#plusBreak").on("click", function() {
-        if (paused) {
-            curBreak++;
-            $("#break").text(curBreak);
-        }
-    });
-
-    $("#minusSession").on("click", function() {
-        if (curSession > 1 && paused) {
-            curSession--;
-            $("#session").text(curSession);
-            $("#timer").text(curSession);
-            curTime = $("#timer").text();
-            t = 0;
-            clock.setHours(0, curTime, 0);
-        }
-    });
-
-    $("#plusSession").on("click", function() {
-        if (paused) {
-            curSession++;
-            $("#session").text(curSession);
-            $("#timer").text(curSession);
-            curTime = $("#timer").text();
-            t = 0;
-            clock.setHours(0, curTime, 0);
-        }
-    });
-
-    $(".circle").on("click", function() {
-        // if paused
-        if (paused) {
-            paused = !paused;
-            t = setTimeout(getTimer, 1);
-
-        // if unpaused
-        } else {
-            paused = !paused;
-            clearTimeout(t);
-        }
-    });
-
-    function getTimer() {
-        var m = clock.getMinutes();
-        var s = clock.getSeconds();
-
-        m= (m<10)?"0"+m: m;
-        s= (s<10)? "0"+s : s;
-
-        $("#timer").text(m+":"+s);
-
-        if (isBreak) {
-            $(".circle").css("border-color", "#40C947");
-        } else {
-            $(".circle").css("border-color", "#AF4848");
-        }
-
-        if (m==0 && s==0) {
-            clearTimeout(t)
-            isBreak = !isBreak;
-
-            // switch to break mode
-            if (isBreak) {
-                t = 0;
-                clock.setHours(0, curBreak, 0);
-                t = setTimeout(getTimer, 1000);
-
-            // switch to timer mode
-            } else {
-                t = 0;
-                clock.setHours(0, curTime, 0);
-                t = setTimeout(getTimer, 1000);
-            }
-            return;   
-        }
-        
-        clock.setSeconds(clock.getSeconds()-1);
-        t = setTimeout(getTimer, 1000);
+  // Click event to decrement the break length
+  $("#minusBreak").on("click", function () {
+    // Decrement if the timer is greater than 1 and is paused
+    if (curBreak > 1 && paused) {
+      curBreak--;
+      $("#break").text(curBreak);
     }
+  });
+
+  // Click event to increment the break length
+  $("#plusBreak").on("click", function () {
+    // Incremement if the timer is paused
+    if (paused) {
+      curBreak++;
+      $("#break").text(curBreak);
+    }
+  });
+
+  // Click event to decrement the session length
+  $("#minusSession").on("click", function () {
+    // Decrement if the timer is greater than 1 and is paused
+    if (curSession > 1 && paused) {
+      curSession--;
+      $("#session").text(curSession);
+      $("#timer").text(curSession);
+      curTime = $("#timer").text();
+      // Reset the timer to 0 and the session to the new session length
+      t = 0;
+      clock.setHours(0, curTime, 0);
+    }
+  });
+
+  $("#plusSession").on("click", function () {
+    // Incremement if the timer is paused
+    if (paused) {
+      curSession++;
+      $("#session").text(curSession);
+      $("#timer").text(curSession);
+      curTime = $("#timer").text();
+      // Reset the timer to 0 and the session to the new session length
+      t = 0;
+      clock.setHours(0, curTime, 0);
+    }
+  });
+
+  // Click event for the main play button
+  $(".circle").on("click", function () {
+    // If currently paused, unpause the timer
+    if (paused) {
+      paused = !paused;
+      t = setTimeout(getTimer, 1);
+    } else {
+      // Otherwise pause it
+      paused = !paused;
+      clearTimeout(t);
+    }
+  });
+
+  // A function that handles the Javascript timer using the global variable t
+  function getTimer() {
+    var m = clock.getMinutes();
+    var s = clock.getSeconds();
+
+    // Add 0's if the times are less than 10 to keep double digits
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    // Create the timer string with the semicolon between minutes and seconds
+    $("#timer").text(m + ":" + s);
+
+    // Change the color of the play circle depending on whether it's a break or countdown
+    if (isBreak) {
+      $(".circle").css("border-color", "#40C947");
+    } else {
+      $(".circle").css("border-color", "#AF4848");
+    }
+
+    // When the timer hits 0, clear it and start the break
+    if (m == 0 && s == 0) {
+      clearTimeout(t)
+      isBreak = !isBreak;
+
+      // If on break, create a timer for the break length and count down from it
+      if (isBreak) {
+        t = 0;
+        clock.setHours(0, curBreak, 0);
+        t = setTimeout(getTimer, 1000);
+      } else {
+        // Otherwise start the timer for the session length
+        t = 0;
+        clock.setHours(0, curTime, 0);
+        t = setTimeout(getTimer, 1000);
+      }
+      return;
+    }
+
+    // Count down every second
+    clock.setSeconds(clock.getSeconds() - 1);
+    t = setTimeout(getTimer, 1000);
+  }
 });
